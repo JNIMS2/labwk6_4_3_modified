8aea8f787bd736525218b37f19cd71f18b9426b4
Jessica Miller-Nims <Jessicamiller-nims@quickenloans.com>
2021-08-16 16:32:00 -0400

    first commit

---
+++ wk6_lab_4_3_Modified_Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace wk6_lab_4_3_Modified_Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ wk6_lab_4_3_modified/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace wk6_lab_4_3_modified
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("test");
+        }
+    }
+}
