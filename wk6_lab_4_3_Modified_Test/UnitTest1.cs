b24156f4683a459993982bd7fa3cdf5dd7557680
Jessica Miller-Nims <Jessicamiller-nims@quickenloans.com>
2021-08-17 14:02:21 -0400

    as far as i got

--- wk6_lab_4_3_Modified_Test/UnitTest1.cs
+++ wk6_lab_4_3_Modified_Test/UnitTest1.cs
@@ -1,14 +1,90 @@
 using System;
 using Xunit;
+using wk6_lab_4_3_Modified_Test;
 
 namespace wk6_lab_4_3_Modified_Test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        /* test for prime numbers in a sequence
+         * 1 is not considered prime. so it will require special conditions
+         * N    Expected
+         * 1    2
+         * 2    3
+         * 3    5
+         * 4    7
+         * 5    11
+         * 6    13
+         * 7    17
+         * 8    19
+         * 9    23
+         * 10   29
+         * 11   31
+         * 12   37
+         * 13   41
+         * 14   43
+         * 15   47
+         * 16   53
+         * 25   97
+        
+         */
+        //[Theory]
+        //[InlineData(1, 2)]
+        //[InlineData(2,3)]
+        //[InlineData(3,5)]
+        //[InlineData(4, 7)]
+        //[InlineData(5, 11)]
+        //[InlineData(6, 13)]
+        //[InlineData(7, 17)]
+        //[InlineData(8, 19)]
+        //[InlineData(9, 23)]
+        //[InlineData(10, 29)]
+        //[InlineData(11, 31)]
+        //[InlineData(12, 37)]
+        //[InlineData(13, 41)]
+        //[InlineData(14, 43)]
+        //[InlineData(15, 47)]
+        //[InlineData(16, 53)]
+        //[InlineData(25, 97)]
 
+        //public void Test1()
+        //{
+
+        //}
+
+
+        /* more sample tests to FIND the primes
+         * do the data THEN the functions
+         * prime numbers are only divisible by 1 AND themselves.
+         * 
+         */
+         [Theory]
+        [InlineData(1, false)]
+        [InlineData(2,true)]
+        [InlineData(3,true)]
+        [InlineData(4, false)]
+        [InlineData(5, true)]
+        [InlineData(6, false)]
+        [InlineData(7, true)]
+        [InlineData(8, false)]
+        [InlineData(9, false)]
+        [InlineData(10, false)]
+        [InlineData(11, true)]
+        [InlineData(12, false)]
+        [InlineData(13, true)]
+        [InlineData(14, false)]
+        [InlineData(15, false)]
+        [InlineData(16, false)]
+        [InlineData(25, false)]
+        [InlineData(47, true)]
+        [InlineData(100, false)]
+         public void TestIsPrime(int num, bool expected)
+        {
+            bool primecheck = wk6_lab_4_3_modified.Program.IsPrime(num);
+            Assert.Equal(expected, primecheck); //it won't call my program here and idk why
         }
+
+        //first test failed half cuz no parameters
+        //i can't get past 13 or 14 passed...
     }
 }
--- wk6_lab_4_3_modified/Program.cs
+++ wk6_lab_4_3_modified/Program.cs
@@ -2,13 +2,77 @@
 
 namespace wk6_lab_4_3_modified
 {
-    class Program
+
+
+
+    public class Program
     {
+        /* i need to:
+         * 1st make a method to FIND prime numbers
+         * 2nd make a method to find the Nth prime
+         * 
+         * 
+         * 
+         * 
+         */
+        //*******************testing jazz  //else***********************
+        //{
+        //    return false;
+        //}
+        //var limit = Math.Ceiling(Math.Sqrt(num));
+        //while (true)
+        //{
+        //    for (int i = 2; i <=limit; i++)
+        //    {
+        //        if (num % i == 0)
+        //        {
+        //            return false;
+        //        }
+        //        return true;
+        //    }
+        //}
+
+        //********i also know this goes a sep class. but, i wanted to get it working first. it did not...
+        public static bool IsPrime(int num)
+        {
+            //special situation for 1 and 0...
+            if (num <=1)
+            {
+                if (num == 2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+
+            for (int i = 3; (i * i) <= num; i += 2)
+            {
+                if ((num % 1) == 0)
+                {
+                    return false;
+                }
+            }
+            return num != 1;
+
+        }
+
+
+
+        //it keeps making everything true
+        //1 makes it a forever loop
+        //got out of the forever loop. now i get 13 passed. but not all prime or not....idk what is happening..
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("test");
-            Console.WriteLine("test2");
+
         }
+
     }
 }
